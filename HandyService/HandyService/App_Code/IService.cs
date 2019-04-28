83f635abbdcf39fd641eccb343a688ce1aa1b36f
MolefiMatla <molefi.matla@dariel.co.za>
2019-04-28 10:50:44 +0200

    Fixed - service bootstrap

---
+++ HandyService/HandyService/App_Code/IService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+[ServiceContract]
+public interface IService
+{
+
+	[OperationContract]
+	Worker RegisterWorker(string email, string password, string firsname, string lastname, double ratebase);
+    [OperationContract]
+    Client RegisterClient(string email, string password, string firsname, string lastname);
+    [OperationContract]
+    User Login(string email, string password);
+    [OperationContract]
+    User Deregister(string email, string password);
+    [OperationContract]
+    bool AddCertificate(string email,string name, string issuedby);
+    [OperationContract]
+    bool RemoveCertificate(string email,string name);
+    [OperationContract]
+    bool AddCategory(string email, string name);
+    [OperationContract]
+    bool RemoveCategory(string email, string name);
+
+
+}
---
+++ HandyService/HandyService/App_Code/Service.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+public class Service : IService
+{
+    handyEntities handyEntities = new handyEntities();
+
+    bool IService.AddCategory(string email, string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool IService.AddCertificate(string email, string name, string issuedby)
+    {
+        throw new NotImplementedException();
+    }
+
+    User IService.Deregister(string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    User IService.Login(string email, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    Client IService.RegisterClient(string email, string password, string firsname, string lastname)
+    {
+        throw new NotImplementedException();
+    }
+
+    Worker IService.RegisterWorker(string email, string password, string firsname, string lastname, double ratebase)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool IService.RemoveCategory(string email, string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    bool IService.RemoveCertificate(string email, string name)
+    {
+        throw new NotImplementedException();
+    }
+}
