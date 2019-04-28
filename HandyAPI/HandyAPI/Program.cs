da6f45c9c7436e5100efd6dd62ebe804e7a43858
Molefi Matla <molefi.matla@headoffice.dariel-solutions>
2019-04-28 10:40:54 +0200

    First bootstrap of the service with entity models

---
+++ HandyAPI/HandyAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HandyAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
