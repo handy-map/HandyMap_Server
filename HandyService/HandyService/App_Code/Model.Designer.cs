fac9b04446fefdc49bc2fee76dc0a819868bb2c7
jason.gordon <jason.gordon@dariel.co.za>
2019-04-28 15:57:06 +0200

    Naming convention changes

--- HandyService/HandyService/App_Code/Model.Designer.cs
+++ HandyService/HandyService/App_Code/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Molefi.Matla\Documents\Visual Studio 2017\Projects\HandyMap_Server\HandyService\HandyService\App_Code\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\jason.gordon\Documents\Hackathon_2019\HandyMap\HandyMap_Server\HandyService\HandyService\App_Code\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
